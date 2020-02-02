fbe12469e6115443b9e76e4c2e0a42d9d9a28715
Grant <grant111924@gmail.com>
2020-02-02 16:08:20 +0800

    create runner

---
+++ main.py
@@ -0,0 +1,24 @@
+import sys,getopt
+# from keras import backend as K
+import torch
+# import utils 
+class ():
+    def __init__(self):
+        pass 
+    def data():
+        pass
+    def train():
+        pass
+    def test():
+        pass
+    def predit():
+        pass
+def main(args):
+    pass 
+
+if __name__ == "__main__":
+    x = torch.rand(5, 3)
+    print(x)
+    # main();
+    pass
+
